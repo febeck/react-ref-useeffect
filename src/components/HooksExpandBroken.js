0ab1842983293da533c719e99988f24a94f22527
Fernando Beck <fe.beck25@gmail.com>
2019-09-29 16:46:32 +0200

    Add auxiliary intermediate files

---
+++ src/components/HooksExpandBroken.js
@@ -0,0 +1,24 @@
+import React, { useRef, useState, useEffect } from 'react'
+import { ReadMore } from './ReadMore'
+
+export function HooksExpandBroken({ height, children }) {
+  const contentRef = useRef()
+  const [isWrapped, setIsWrapped] = useState(false)
+  const [hasUserUnwrapped, setHasUserUnwrapped] = useState(false)
+
+  useEffect(() => {
+    if (hasUserUnwrapped || isWrapped || !contentRef.current) return
+    if (contentRef.current.clientHeight > height) setIsWrapped(true)
+  }, [contentRef, hasUserUnwrapped, height, isWrapped])
+
+  function handleExpand() {
+    setIsWrapped(false)
+    setHasUserUnwrapped(true)
+  }
+
+  return (
+    <ReadMore isWrapped={isWrapped} height={height} handleExpand={handleExpand}>
+      {React.cloneElement(children, { ref: contentRef })}
+    </ReadMore>
+  )
+}
---
+++ src/components/HooksExpandFinal.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect} from 'react'
+import { ReadMore } from './ReadMore'
+
+export function HooksExpandFinal({ height, children }) {
+  const [isWrapped, setIsWrapped] = useState(false)
+  const [hasUserUnwrapped, setHasUserUnwrapped] = useState(false)
+
+  const [contentHeight, setContentHeight] = useState()
+
+  const contentRef = (node) => {
+    if (node !== null) {
+      setContentHeight(node.getBoundingClientRect().height);
+    }
+  }
+
+  useEffect(() => {
+    if (hasUserUnwrapped || isWrapped) return
+    setIsWrapped(contentHeight > height)
+  }, [contentHeight, hasUserUnwrapped, height, isWrapped])
+
+
+  function handleExpand() {
+    setIsWrapped(false)
+    setHasUserUnwrapped(true)
+  }
+
+  return (
+    <ReadMore isWrapped={isWrapped} height={height} handleExpand={handleExpand}>
+      {React.cloneElement(children, { ref: contentRef })}
+    </ReadMore>
+  )
+}
